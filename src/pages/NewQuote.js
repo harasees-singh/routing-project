4ccbafdcbfff6be703f7d89bd9151215e977b4e3
Harasees Singh <76965050+harasees-singh@users.noreply.github.com>
2022-09-20 21:29:48 +0530

    add new quote working

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,12 +1,21 @@
 import { useHistory } from 'react-router-dom';
 import QuoteForm from '../components/quotes/QuoteForm'
+import useHttp from '../hooks/hooks/use-http';
+import { useEffect } from 'react';
+import { addQuote } from '../lib/lib/api';
 const NewQuote = () => {
     const history = useHistory();
-    const addQuoteHandler = quoteData => {
-        console.log(quoteData);
+    const {sendRequest, status} = useHttp(addQuote);
 
-        history.push('/quotes')
+    useEffect( () => {
+        if(status === 'completed')
+            history.push('/quotes');
+    }, [status, history]);
+
+    const addQuoteHandler = quoteData => {
+        sendRequest(quoteData);
+
     }
-    return <QuoteForm onAddQuote={addQuoteHandler}/>    
+    return <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler}/>    
 }
 export default NewQuote;
